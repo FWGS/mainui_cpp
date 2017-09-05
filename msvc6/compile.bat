ef99c3ee5742fbadeff0b5fda72839d9322abc9b
Alibek Omarov (a1batross) <a1ba.omarov@gmail.com>
2017-09-05 22:13:37 +0300

    compile.bat script from mainui_qwrap

--- msvc6/compile.bat
+++ msvc6/compile.bat
@@ -1,3 +1,23 @@
-..\vsmini\bin\vc
-cl /DXASH_DISABLE_FWGS_EXTENSIONS /DMY_COMPILER_SUCKS user32.lib ..\*.cpp ..\controls\*.cpp ..\menus\*.cpp ..\menus\dynamic\*.cpp -I..\ -I..\controls -I..\menus -I..\..\xash3d -I..\..\xash3d\engine -I..\..\xash3d\pm_shared -I..\..\xash3d\common -o menu.dll /link /dll /out:menu.dll
-del *.obj
+@echo off
+echo Setting environment for minimal Visual C++ 6
+set MSVCDir=Z:\home\a1ba\projects\Xash3D\mainui_cpp\vsmini
+set INCLUDE=%MSVCDir%\include
+set LIB=%MSVCDir%\lib
+set PATH=%MSVCDir%\bin;%PATH%
+
+echo -- Compiler is MSVC6
+
+set XASH3DSRC=..\..\Xash3D_original
+set INCLUDES=-I..\ -I..\font -I..\controls -I..\menus -I%XASH3DSRC% -I%XASH3DSRC%\engine -I%XASH3DSRC%\pm_shared -I%XASH3DSRC%\common -I..\utl
+set SOURCES=..\*.cpp ..\controls\*.cpp ..\menus\*.cpp ..\font\*.cpp ..\menus\dynamic\*.cpp
+set DEFINES=/DMY_COMPILER_SUCKS /DPRERELEASE_INTERFACE /DMAINUI_USE_CUSTOM_FONT_RENDER /DXASH_DISABLE_FWGS_EXTENSIONS /Dstrcasecmp=stricmp
+set LIBS=user32.lib gdi32.lib
+set OUTNAME=menu.dll
+;set DEBUG=/debug
+
+cl %DEFINES% %LIBS% %SOURCES% %INCLUDES% -o %OUTNAME% /link /dll /out:%OUTNAME% %DEBUG%
+
+echo -- Compile done. Cleaning...
+
+del *.obj *.exp *.lib *.ilk
+echo -- Done.
