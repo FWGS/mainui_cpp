c7cd8f6cf1f55e8cfc5ab21b79a044b81dcb21f4
Alibek Omarov <a1ba.omarov@gmail.com>
2018-03-12 23:31:03 +0300

    Update msvc6 compile bat

--- msvc6/compile.bat
+++ msvc6/compile.bat
@@ -1,21 +1,22 @@
 @echo off
+
 echo Setting environment for minimal Visual C++ 6
-set MSVCDir=Z:\home\a1ba\projects\Xash3D\mainui_cpp\vsmini
+set MSVCDir=C:\Program Files (x86)\Microsoft Visual Studio\VC98\
 set INCLUDE=%MSVCDir%\include
 set LIB=%MSVCDir%\lib
 set PATH=%MSVCDir%\bin;%PATH%
 
 echo -- Compiler is MSVC6
 
-set XASH3DSRC=..\..\Xash3D_original
+set XASH3DSRC=..\..\..\Xash3D_original
 set INCLUDES=-I..\ -I..\font -I..\controls -I..\menus -I%XASH3DSRC% -I%XASH3DSRC%\engine -I%XASH3DSRC%\pm_shared -I%XASH3DSRC%\common -I..\utl
 set SOURCES=..\*.cpp ..\controls\*.cpp ..\menus\*.cpp ..\font\*.cpp ..\menus\dynamic\*.cpp
 set DEFINES=/DMY_COMPILER_SUCKS /DPRERELEASE_INTERFACE /DMAINUI_USE_CUSTOM_FONT_RENDER /DXASH_DISABLE_FWGS_EXTENSIONS /Dstrcasecmp=stricmp
-set LIBS=user32.lib gdi32.lib
+set LINKLIBS=gdi32.lib user32.lib
 set OUTNAME=menu.dll
-;set DEBUG=/debug
+rem set DEBUG=/debug
 
-cl %DEFINES% %LIBS% %SOURCES% %INCLUDES% -o %OUTNAME% /link /dll /out:%OUTNAME% %DEBUG%
+cl %DEFINES% %SOURCES% %INCLUDES% -o %OUTNAME% /link /dll %LINKLIBS% /out:%OUTNAME% %DEBUG%
 
 echo -- Compile done. Cleaning...
 
