d6110913a30bf65f98a3b5f7770ed0614a8e35f5
Alibek Omarov <a1ba.omarov@gmail.com>
2018-05-06 12:35:42 +0300

    Change colors defauls to match WON colors

--- BaseMenu.cpp
+++ BaseMenu.cpp
@@ -55,12 +55,13 @@ const char	*uiSoundRemoveKey   = "media/launch_deny1.wav";
 const char	*uiSoundMove        = "";		// Xash3D not use movesound
 const char	*uiSoundNull        = "";
 
-int		uiColorHelp         = 0xFFFFFFFF;	// 255, 255, 255, 255	// hint letters color
-int		uiPromptBgColor     = 0x80404040;	// 64,  64,  64,  255	// dialog background color
-int		uiPromptTextColor   = 0xFFF0B418;	// 255, 160,  0,  255	// dialog or button letters color
+// they match default WON colors.lst now, except alpha
+int		uiColorHelp         = 0xFF7F7F7F;	// 127, 127, 127, 255	// hint letters color
+int		uiPromptBgColor     = 0xFF383838;	// 56,  56,  56,  255	// dialog background color
+int		uiPromptTextColor   = 0xFFF0B418;	// 240, 180, 24,  255	// dialog or button letters color
 int		uiPromptFocusColor  = 0xFFFFFF00;	// 255, 255,  0,  255	// dialog or button focus letters color
-int		uiInputTextColor    = 0xFFC0C0C0;	// 192, 192, 192, 255
-int		uiInputBgColor      = 0x80404040;	// 64,  64,  64,  255	// field, scrollist, checkbox background color
+int		uiInputTextColor    = 0xFFF0B418;	// 240, 180, 24, 255
+int		uiInputBgColor      = 0x80383838;	// 56,  56,  56,  255	// field, scrollist, checkbox background color
 int		uiInputFgColor      = 0xFF555555;	// 85,  85,  85,  255	// field, scrollist, checkbox foreground color
 int		uiColorWhite        = 0xFFFFFFFF;	// 255, 255, 255, 255	// useful for bitmaps
 int		uiColorDkGrey       = 0x80404040;	// 64,  64,  64,  255	// shadow and grayed items
