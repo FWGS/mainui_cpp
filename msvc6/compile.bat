7b967aca8eea9baf933f85088160347dffc68e08
Alibek Omarov <a1ba.omarov@gmail.com>
2019-01-12 02:50:56 +0300

    msvc6: compile.bat: fix path to miniutl

--- msvc6/compile.bat
+++ msvc6/compile.bat
@@ -9,8 +9,8 @@ set PATH=%MSVCDir%\bin
 echo -- Compiler is MSVC6
 
 set XASH3DSRC=Xash3D_original
-set INCLUDES=-I..\ -I..\model -I..\font -I..\controls -I..\menus -I%XASH3DSRC% -I%XASH3DSRC%\engine -I%XASH3DSRC%\pm_shared -I%XASH3DSRC%\common -I%XASH3DSRC%\cl_dll -I..\utl
-set SOURCES=..\*.cpp ..\controls\*.cpp ..\menus\*.cpp ..\font\*.cpp ..\menus\dynamic\*.cpp ..\utl\*.cpp
+set INCLUDES=-I..\ -I..\model -I..\font -I..\controls -I..\menus -I%XASH3DSRC% -I%XASH3DSRC%\engine -I%XASH3DSRC%\pm_shared -I%XASH3DSRC%\common -I%XASH3DSRC%\cl_dll -I..\miniutl
+set SOURCES=..\*.cpp ..\controls\*.cpp ..\menus\*.cpp ..\font\*.cpp ..\menus\dynamic\*.cpp ..\miniutl\*.cpp
 set DEFINES=/DMY_COMPILER_SUCKS /DNEW_ENGINE_INTERFACE /DMAINUI_USE_CUSTOM_FONT_RENDER /DXASH_DISABLE_FWGS_EXTENSIONS /Dstrcasecmp=stricmp
 set LINKLIBS=gdi32.lib user32.lib
 set OUTNAME=menu.dll
