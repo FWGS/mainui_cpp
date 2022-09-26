461a0488e6701524b05debf7ac170eb5dbee39c8
Alibek Omarov <a1ba.omarov@gmail.com>
2022-09-26 13:54:56 +0300

    Primitive: deprecate QMF_EVENTSIGNOREFOCUS, it's not used anywhere

--- Primitive.h
+++ Primitive.h
@@ -62,7 +62,7 @@ enum
 	QMF_HASKEYBOARDFOCUS   = BIT( 11 ),
 	QMF_DIALOG             = BIT( 12 ), // modal windows. Will grab key, char and mousemove events
 	QMF_DISABLESCAILING    = BIT( 13 ), // disables CalcPosition and CalcSizes
-	QMF_EVENTSIGNOREFOCUS  = BIT( 14 ), // don't care if item have focus, it must get events anyway
+	// deprecated: QMF_EVENTSIGNOREFOCUS  = BIT( 14 ), // don't care if item have focus, it must get events anyway
 
 	QMF_CLOSING            = BIT( 29 ), // INTERNAL USE ONLY: window is closing right now and we need only draw animation
 	QMF_HIDDENBYPARENT     = BIT( 30 ), // INTERNAL USE ONLY: parent set this flag and don't want to draw this control
