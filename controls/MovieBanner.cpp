149788099aa39062ab102bd5adbedde032dcf11a
Razcoina <pedrojcd22@gmail.com>
2026-02-06 20:58:08 +0000

    MovieBanner: align the banner with the background image

--- controls/MovieBanner.cpp
+++ controls/MovieBanner.cpp
@@ -19,23 +19,58 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 #include "MovieBanner.h"
 
+// In WON menus the background is always 640x480
+const float splashWidth = 640.0f;
+const float splashHeight = 480.0f;
+
 void CMenuMovieBanner::VidInit()
 {
 	EngFuncs::PrecacheLogo( "logo.avi" );
 
-	float scaleX = ScreenWidth / 640.0f;
-	float scaleY = ScreenHeight / 480.0f;
+	float xScale, yScale;
+	float xOffset = 0, yOffset = 0;
 
-	m_scPos.x = 0;
-	m_scPos.y = 70 * scaleY * uiStatic.scaleY; // 70 is empirically determined value (magic number)
+	// Adapt to stretched or cropped background
+	if( ui_background_stretch->value )
+	{
+		xScale = ScreenWidth / splashWidth;
+		yScale = ScreenHeight / splashHeight;
+	}
+	else
+	{
+		if( ScreenWidth * splashHeight > ScreenHeight * splashWidth )
+		{
+			xScale = ScreenWidth / splashWidth;
+			yScale = xScale;
 
-	// a1ba: multiply by height scale to look better on wide screens
-	m_scSize.w = EngFuncs::GetLogoWidth() * scaleX;
-	m_scSize.h = EngFuncs::GetLogoHeight() * scaleY * uiStatic.scaleY;
+			yOffset = ( ScreenHeight - 480.0f * yScale ) / 2;
+		}
+		else
+		{
+			yScale = ScreenHeight / splashHeight;
+			xScale = yScale;
+
+			xOffset = ( ScreenWidth - 640.0f * xScale ) / 2;
+		}
+	}
+
+	m_scPos.x = (int)xOffset;
+	m_scPos.y = (int)( yOffset + 70.0f * yScale ); // 70 is empirically determined value (magic number)
+
+	m_scSize.w = (int)( EngFuncs::GetLogoWidth( ) * xScale );
+	m_scSize.h = (int)( EngFuncs::GetLogoHeight( ) * yScale );
 }
 
 void CMenuMovieBanner::Draw()
 {
+	if( FBitSet( ui_background_stretch->flags, FCVAR_CHANGED ) )
+	{
+		ClearBits( ui_background_stretch->flags, FCVAR_CHANGED );
+
+		// Logo needs to be reset when this CVAR changes
+		VidInit();
+	}
+
 	if( EngFuncs::ClientInGame() && EngFuncs::GetCvarFloat( "ui_renderworld" ))
 		return;
 
